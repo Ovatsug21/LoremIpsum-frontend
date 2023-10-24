9957644fe9fdb0cc9dd3684ae894bfa756d6fa67
Gustavo Gonçalves <guga1507@hotmail.com>
2023-10-24 03:39:54 -0300

    components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClienteComponent } from './components/cliente/cliente.component';
+import { EnderecoComponent } from './components/endereco/endereco.component';
+import { ClienteFormComponent } from './components/cliente-form/cliente-form.component';
+import { EnderecoFormComponent } from './components/endereco-form/endereco-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClienteComponent,
+    EnderecoComponent,
+    ClienteFormComponent,
+    EnderecoFormComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/endereco-form/endereco-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-endereco-form',
+  templateUrl: './endereco-form.component.html',
+  styleUrls: ['./endereco-form.component.scss']
+})
+export class EnderecoFormComponent {
+
+}
